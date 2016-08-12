9e18eec46d0348d39dbc8aceecbe7e4b6b583a7a
sunny5156 <sunny5156@qq.com>
2016-08-12 16:23:09 +0800

    消息 api

---
+++ Message/MessageAPI.php
@@ -0,0 +1,11 @@
+<?php
+namespace Message;
+
+class MessageAPI {
+    
+    //发送消息
+    private $messageSendURL = "https://qyapi.weixin.qq.com/cgi-bin/message/send?access_token=ACCESS_TOKEN";
+    
+    //
+    
+}
